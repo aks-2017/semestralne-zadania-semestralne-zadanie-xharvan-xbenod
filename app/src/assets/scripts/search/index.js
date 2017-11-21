cef43fca41c055dcc0df9e80158e627ddaaef49d
Simon Harvan <simon.harvan@pixwell.sk>
2017-11-21 11:04:28 +0100

    First API call

--- app/src/assets/scripts/search/index.js
+++ app/src/assets/scripts/search/index.js
@@ -1,9 +1,19 @@
 import * as $ from 'jquery';
 
 export default (function () {
-  $('.search-toggle').on('click', e => {
-    $('.search-box, .search-input').toggleClass('active');
-    $('.search-input input').focus();
-    e.preventDefault();
-  });
+    $('.make-call').on('click', e => {
+        $.ajax({
+            method: 'GET',
+            url: 'http://192.168.56.101:8080/firewall/module/status',
+            datatype: 'jsonp',
+            crossDomain: true,
+            success: function (data) {
+                alert("Success");
+                console.log(data);
+            },
+            error: function () {
+                alert('Failed!');
+            },
+        });
+    });
 }());
