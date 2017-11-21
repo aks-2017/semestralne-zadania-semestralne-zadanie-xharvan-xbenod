56f37d74c905fdff667e4b07617d79a7b7b8a845
Dávid Beňo <dawid.beno@gmail.com>
2017-11-21 14:36:21 +0100

    switch status

--- app/src/assets/scripts/search/index.js
+++ app/src/assets/scripts/search/index.js
@@ -9,6 +9,25 @@ export default (function () {
             crossDomain: true,
             success: function (data) {
                 alert("Success");
+                //var switchStatus = "Switch id: "+data[0].switch_id+" is "+data[0].status;
+                var ol = document.getElementById('switch_status_list');
+              /*  for(var i=1; i<data.length; i++){
+                  var id = "sw";
+                  id += i;
+                  id = id.toString();
+                  var element = "<li id=\""+id+"\"></li>"
+                  ol.append(element.toString());
+                }*/
+
+                for(var i=1; i<data.length+1; i++){
+                  var switchStatus = "Switch id: "+data[i-1].switch_id+" is "+data[i-1].status;
+                  var id = "sw";
+                  id += i;
+                  id = id.toString();
+                  var p = document.getElementById(id);
+                  p.innerHTML += switchStatus;
+                }
+
                 console.log(data);
             },
             error: function () {
