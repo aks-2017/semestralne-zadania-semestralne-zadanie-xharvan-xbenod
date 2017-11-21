525f30c8d527c7d675a3d0d3a8e04fe7ca8a3b48
Simon Harvan <simon.harvan@pixwell.sk>
2017-11-21 18:48:05 +0100

    Add switches table and ip in cookies

--- app/src/assets/scripts/search/index.js
+++ app/src/assets/scripts/search/index.js
@@ -1,38 +1,75 @@
 import * as $ from 'jquery';
 
+
+function initIp() {
+    if (Cookies.get('ip_saved') === true) {
+        $('.ip-address').css("display", "none")
+    }else {
+        $('.switch').css("display", "none")
+    }
+}
+
 export default (function () {
-    $('.make-call').on('click', e => {
-        $.ajax({
-            method: 'GET',
-            url: 'http://192.168.56.101:8080/firewall/module/status',
-            datatype: 'jsonp',
-            crossDomain: true,
-            success: function (data) {
-                alert("Success");
-                //var switchStatus = "Switch id: "+data[0].switch_id+" is "+data[0].status;
-                var ol = document.getElementById('switch_status_list');
-              /*  for(var i=1; i<data.length; i++){
-                  var id = "sw";
-                  id += i;
-                  id = id.toString();
-                  var element = "<li id=\""+id+"\"></li>"
-                  ol.append(element.toString());
-                }*/
-
-                for(var i=1; i<data.length+1; i++){
-                  var switchStatus = "Switch id: "+data[i-1].switch_id+" is "+data[i-1].status;
-                  var id = "sw";
-                  id += i;
-                  id = id.toString();
-                  var p = document.getElementById(id);
-                  p.innerHTML += switchStatus;
-                }
-
-                console.log(data);
-            },
-            error: function () {
-                alert('Failed!');
-            },
-        });
+    $(document).ready(e => {
+        initIp();
+        initDashboard();
     });
+
+    $('.enable-switch-all').click(function () {
+        enableSwitch('enable', 'all');
+    })
+
+
 }());
+
+function enableSwitch(enable, switchId) {
+    $.ajax({
+        method: 'PUT',
+        url: 'http://192.168.56.101:8080/firewall/module/' + enable + '/' + switchId,
+        datatype: 'jsonp',
+        crossDomain: true,
+        success: function (data) {
+            initDashboard();
+            console.log(data);
+        },
+        error: function (data) {
+            alert('Failed!');
+            console.log(data);
+        },
+    });
+}
+
+function initDashboard() {
+    $.ajax({
+        method: 'GET',
+        url: 'http://192.168.56.101:8080/firewall/module/status',
+        datatype: 'jsonp',
+        crossDomain: true,
+        success: function (data) {
+            parseResponse(data);
+            console.log(data);
+        },
+        error: function (data) {
+            alert('Failed!');
+            console.log(data);
+        },
+    });
+}
+
+function parseResponse(json) {
+
+
+    $('.switch-body').remove();
+    $('.switch-table').append('<tbody class="switch-body"></tbody>');
+    let switchTable = $('.switch-body');
+    for (let i = 0; i < json.length; i++) {
+        let enable = 'enable';
+        if (json[i].status === 'enable') {
+            enable = 'disable'
+        }
+        switchTable.append('<tr id="switch-' + json[i].switch_id + '"><td>' + json[i].switch_id + '</td><td>' + json[i].status + 'd</td><td><a href="#" class="btn btn-sm btn-primary enable-switch-' + json[i].switch_id + '">' + enable + '</a></td></td></tr>')
+        switchTable.find('.enable-switch-' + json[i].switch_id).click(function (e) {
+            enableSwitch(enable, json[i].switch_id);
+        })
+    }
+}
